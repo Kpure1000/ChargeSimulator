5ae11f1876a104897f508307bb498481488c22f8
Kpurek <ktsq2000@126.com>
2020-05-09 15:57:04 +0800

    Fixed problem of Stack Flow in Property<-curDayTime.

--- Simulation/Assets/Scripts/DayTimerController.cs
+++ Simulation/Assets/Scripts/DayTimerController.cs
@@ -6,31 +6,55 @@ using UnityEngine.UI;
 
 public class DayTimerController : MonoBehaviour
 {
+    /// <summary>
+    /// 当前实际时间
+    /// </summary>
     public int curTime { get; set; }
-
+    /// <summary>
+    /// 最大实际时间
+    /// </summary>
+    private const int maxCurTime = 1440;
+    /// <summary>
+    /// 当前格式时间（只读）
+    /// </summary>
+    public Vector2Int curDayTime
+    {
+        get { return new Vector2Int(curTime / 60, curTime % 60); }
+    }
+    /// <summary>
+    /// 初始化时间
+    /// </summary>
+    public Vector2Int initCurDayTime;
+    /// <summary>
+    /// 引用时间显示UI
+    /// </summary>
     public GameObject timeText;
 
     private void Start()
     {
-        curTime = 0;
-        this.InvokeRepeating("dayTimeUpdate", 0f, 1f);
-    }
+        curTime = 60 * initCurDayTime.x + initCurDayTime.y;
 
+        InvokeRepeating("dayTimeUpdate", 0f, 1f);
+    }
+    /// <summary>
+    /// 增量重复监听方法
+    /// </summary>
     void dayTimeUpdate()
     {
         curTime++;
+        curTime %= maxCurTime;
     }
 
     private void Update()
     {
-
         timeTextUpdate();
     }
-
+    /// <summary>
+    /// 更新时间显示UI的时间
+    /// </summary>
     private void timeTextUpdate()
     {
         if (timeText == null) return;
-        timeText.GetComponent<Text>().text =
-            string.Format("{0}:{1}", curTime, curTime);
+        timeText.GetComponent<Text>().text = string.Format("{0:00}:{1:00}", curDayTime.x, curDayTime.y);
     }
 }
