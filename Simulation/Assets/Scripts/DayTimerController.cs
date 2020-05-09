19b791be0da0e7e3127dec013ccb376d7f4b8706
Kpurek <ktsq2000@126.com>
2020-05-09 12:38:44 +0800

    Commit class DayTimeController, need to adapt project.

---
+++ Simulation/Assets/Scripts/DayTimerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayTimerController : MonoBehaviour
+{
+    public int curTime { get; set; }
+
+    public GameObject timeText;
+
+    private void Start()
+    {
+        curTime = 0;
+        this.InvokeRepeating("dayTimeUpdate", 0f, 1f);
+    }
+
+    void dayTimeUpdate()
+    {
+        curTime++;
+    }
+
+    private void Update()
+    {
+
+        timeTextUpdate();
+    }
+
+    private void timeTextUpdate()
+    {
+        if (timeText == null) return;
+        timeText.GetComponent<Text>().text =
+            string.Format("{0}:{1}", curTime, curTime);
+    }
+}
